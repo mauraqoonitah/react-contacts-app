2a98db5965664c4ce5feb124b0ea2baa91d77082
mauraqoonitah <mauraqoonitah@gmail.com>
2023-10-18 16:06:27 +0700

    Feat: case-memberikan styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import ContactApp from './ContactApp';
+import ContactApp from './components/ContactApp';
 
+import './styles/style.css';
 const element = <h1>Hello, Maura!</h1>;
 
 const root = createRoot(document.getElementById('root'));
